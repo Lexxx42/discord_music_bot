6ae97be85ae024799cd00873d6a6fe457729ffe9
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-03 18:44:52 +0300

    added logging into file

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 import os
 
 import discord
+import logging.handlers
 from dotenv import load_dotenv
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -26,4 +43,4 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
-client.run(TOKEN)
+client.run(TOKEN, log_handler=None)
