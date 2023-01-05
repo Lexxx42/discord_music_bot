8526c8b9054bcf5851889a94f3bb9476bcc0a7bc
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-05 13:25:09 +0300

    ill rewrite almost everything music playing not working new libraries requirements.txt need update

--- bot/handlers/users_events.py
+++ bot/handlers/users_events.py
@@ -1,5 +1,7 @@
 """This module is for user's handles."""
 import logging
+import discord
+
 from .. import client
 
 
@@ -17,3 +19,22 @@ async def on_message(message):
     if message.content.startswith('https://www.youtube.'):
         await message.channel.send('Hello!')
         logging.info("sent message: %s", message.content)
+        channel = message.channel.id
+        print(channel)
+        # await channel.connect()
+
+
+@client.command(name='join', aliases=['summon'])  # CREATING COMMAND "JOIN" WITH ALIAS SUMMON
+async def _join(ctx, *,
+                channel: discord.VoiceChannel = None):  # TAKING ARGUMENT CHANNEL SO PPL CAN MAKE THE BOT JOIN A VOICE CHANNEL THAT THEY ARE NOT IN
+    """Joins a voice channel."""
+
+    destination = channel if channel else ctx.author.voice.channel  # CHOOSING THE DESTINATION, MIGHT BE THE REQUESTED ONE, BUT IF NOT THEN WE PICK AUTHORS VOICE CHANNEL
+
+    if ctx.voice_client:  # CHECKING IF THE BOT IS PLAYING SOMETHING
+        await ctx.voice_state.voice.move_to(
+            destination)  # IF THE BOT IS PLAYING WE JUST MOVE THE BOT TO THE DESTINATION
+        return
+
+    await destination.connect()  # CONNECTING TO DESTINATION
+    await ctx.send(f"Succesfully joined the voice channel: {destination.name} ({destination.id}).")
